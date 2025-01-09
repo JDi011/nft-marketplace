65348019ef64ba21b4578461bfb2c503cfab2b3c
Jugen Dharia <jugendharia5@gmail.com>
2025-01-09 11:08:40 +0000

    baseLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import  Navbar  from "../components/navbar";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
